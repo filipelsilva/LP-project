b5f1ff622ffc7b66d87f6e4168b557d45fccaece
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2023-05-22 18:24:38 +0100

    working of the last proof. two first done

--- P1-PL/Interpreter.v
+++ P1-PL/Interpreter.v
@@ -24,10 +24,11 @@ From FirstProject Require Import RelationalEvaluation.
 
 Notation "'LETOPT' x <== e1 'IN' e2"
    := (match e1 with
-         | Some (x, _) => e2
+         | Some (x, SContinue) => e2
+         | Some (x, SBreak) => e1
          | None => None
        end)
-   (right associativity, at level 60).
+   (right associativity, at level 60, only parsing).
 
 (** 2.1. Implement ceval_step as specified. To improve readability,
          you are strongly encouraged to define auxiliary notation.
@@ -67,7 +68,6 @@ Example example_test_ceval :
      = Some (2, 0, 4).
 Proof. reflexivity. Qed.
 
-
 (**
   2.2. TODO: Prove the following three properties.
              Add a succint explanation in your own words of why `equivalence1` and `inequivalence1` are valid.
@@ -76,33 +76,49 @@ Theorem equivalence1: forall st c,
   (exists i0, forall i1, i1>=i0 ->
   ceval_step st <{ break; c }> i1 = ceval_step st <{ break; skip }> i1).
 Proof.
-  (* intros. induction c. *)
-  (* - exists 0. intros. reflexivity. *)
-  (*   - exists 0. intros. induction i1. *)
-  (*   + reflexivity. *)
-  (*   + admit. *)
-  (* - exists 3. intros. reflexivity. *)
-  (* - exists 1. intros. simpl. reflexivity. *)
-(* Qed. *)
-Admitted.
+  intros. exists 2. intros.
+  destruct i1; try lia.
+  destruct i1; try lia.
+  simpl. reflexivity.
+Qed.
 
-(* TODO *)
 Theorem inequivalence1: forall st c,
   (exists i0, forall i1, i1>=i0 ->
   ceval_step st <{ break; c }> i1 <> ceval_step st <{ skip }> i1).
 Proof.
-(* Qed. *)
-Admitted.
+  intros. exists 2. intros.
+  destruct i1; try lia.
+  destruct i1; try lia.
+  simpl. discriminate.
+Qed.
 
 (* TODO *)
 Theorem p1_equivalent_p2: forall st,
   (exists i0,
     forall i1, i1>=i0 ->
-      ceval_step st p1 i1 = ceval_step st p2 i1
+    ceval_step st p1 i1 = ceval_step st p2 i1
   ).
 Proof.
-  (* TODO *)
-(* Qed. *)
-Admitted.
+  intros. exists 6. intros.
+  destruct i1; try lia.
+  destruct i1; try lia.
+  destruct i1; try lia.
+  destruct i1; try lia.
+  destruct i1; try lia.
+  destruct i1; try lia.
+  (* simpl. discriminate. *)
+Qed.
 
+Definition p1 :=
+  X := 1;
+  Y := 0;
+  while true do
+    if X=0 then break else Y := Y+1; X := X-1 end
+  end.
 
+Definition p2 :=
+  X := 1;
+  Y := 0;
+  while ~(X = 0) do
+    Y := Y+1; X := X-1
+  end.
