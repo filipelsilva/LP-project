774a67efe2a29d63016fccd6ff5a1f286da1cff1
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2023-05-25 11:49:14 +0100

    some more admits removed

--- P1-PL/AdditionalProperties.v
+++ P1-PL/AdditionalProperties.v
@@ -142,7 +142,7 @@ Proof.
            +++ (* r1 = Some s *)
                destruct p; destruct r; destruct res.
                ++++ apply (E_WhileTrue_Continue st s st' b c); try apply IHi'; assumption.
-               ++++ apply IHi'. admit.
+               ++++ admit.
                ++++ apply E_WhileTrue_Break.
                     +++++ assumption.
                     +++++ apply IHi'. inversion H1. rewrite H0 in Heqr1. assumption.
@@ -176,11 +176,11 @@ Proof.
       ++ lia.
       ++ destruct res.
          +++ assumption.
-         +++ rewrite H1. admit.
-    + rewrite H3. destruct res.
-      ++ apply (ceval_step_more x).
-         +++ lia.
          +++ admit.
+    + rewrite H3. destruct res.
+      ++ apply (ceval_step_more x0).
+         +++ lia.
+         +++ assumption.
       ++ admit.
   - destruct IHceval. exists (S x). simpl. rewrite H0. reflexivity.
   - destruct IHceval1. destruct IHceval2. exists (S (x+x0)). simpl.
@@ -188,10 +188,15 @@ Proof.
     + apply (ceval_step_more x).
       ++ lia.
       ++ assumption.
-    + rewrite H4. apply (ceval_step_more x).
+    + rewrite H4. apply (ceval_step_more x0).
       ++ lia.
-      ++ admit.
-  - destruct IHceval. exists (S x). simpl. rewrite H. admit.
+      ++ assumption.
+  - destruct IHceval. exists (S x). simpl. rewrite H.
+    assert (ceval_step st c x = Some (st', SBreak)).
+    + apply (ceval_step_more x).
+      ++ lia.
+      ++ assumption.
+    + rewrite H1. reflexivity.
   - exists 1. simpl. rewrite H. reflexivity.
 (* Qed. *)
 Admitted.
